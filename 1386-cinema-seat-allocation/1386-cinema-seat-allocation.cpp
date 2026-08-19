986cb6194cb0b06bba99a7fbb68a4144c0a40e5c
Vikram <vikramnegi0021@gmail.com>
2026-08-19 23:12:39 +0530

    Time: 86 ms (26.52%), Space: 92 MB (10.47%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,33 +1,50 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        unordered_map<int,unordered_set<int>>mp;
-        for(auto&s: reservedSeats){
 
-            int row=s[0];
-            int seat=s[1];
+        unordered_map<int, unordered_set<int>> mp;
+
+        // Reserved seats store karna
+        for (auto &s : reservedSeats) {
+            int row = s[0];
+            int seat = s[1];
+
             mp[row].insert(seat);
         }
-         int result=(n-mp.size())*2;
-         for(auto &[row,bookedseats]:mp){
-            auto isAvaliable=[&](int seat){
-                return bookedseats.find(seat)==bookedseats.end();
-            };
-            bool groupA =isAvaliable(2)&&isAvaliable(3)&&isAvaliable(4)&&isAvaliable(5);
-             bool groupB =isAvaliable(4)&&isAvaliable(5)&&isAvaliable(6)&&isAvaliable(7);
-              bool groupC =isAvaliable(6)&&isAvaliable(7)&&isAvaliable(8)&&isAvaliable(9);
-              if(groupA &&groupC){
-                result+=2;
-              }
-              else if(groupA ||groupB||groupC){
-                result+=1;
-              }
-              else{
-                result+=0;
-              }
-              
 
-         }
-         return result;
+        // Jis row mein koi reservation nahi hai,
+        // usme 2 families aa sakti hain
+        int result = (n - mp.size()) * 2;
+
+        // Sirf reserved wali rows check karenge
+        for (auto &[row, bookedseats] : mp) {
+
+            bool groupA = bookedseats.count(2) == 0 &&
+                          bookedseats.count(3) == 0 &&
+                          bookedseats.count(4) == 0 &&
+                          bookedseats.count(5) == 0;
+
+            bool groupB = bookedseats.count(4) == 0 &&
+                          bookedseats.count(5) == 0 &&
+                          bookedseats.count(6) == 0 &&
+                          bookedseats.count(7) == 0;
+
+            bool groupC = bookedseats.count(6) == 0 &&
+                          bookedseats.count(7) == 0 &&
+                          bookedseats.count(8) == 0 &&
+                          bookedseats.count(9) == 0;
+
+            if (groupA && groupC) {
+                result += 2;
+            }
+            else if (groupA || groupB || groupC) {
+                result += 1;
+            }
+            else {
+                result += 0;
+            }
+        }
+
+        return result;
     }
 };
